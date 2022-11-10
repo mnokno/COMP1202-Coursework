5bb0122aa586a3e2ed44fea64643b96aa4b10a89
mnokno <prv.nowacki@gmail.com>
2022-11-10 19:39:19 +0000

    Created a person class

---
+++ Orchestra/src/people/Person.java
@@ -0,0 +1,20 @@
+package people;
+
+public class Person {
+
+    private final String name;
+
+    /**
+     * Constructor for Person class
+     * @param name name of this person
+     */
+    public Person(String name){
+        this.name = name;
+    }
+
+    /**
+     * Getter for name
+     * @return name of this person
+     */
+    public String getName() {return name;}
+}
